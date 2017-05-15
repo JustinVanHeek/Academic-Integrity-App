cef82fa807d519a7bca2e8f1254a11388a9b4897
dylanblok <Dylan.Blok@student.twu.ca>
2017-05-15 14:18:18 -0700

    About page

--- about.php
+++ about.php
@@ -1,20 +1,28 @@
+<?php
+	session_start();
+
+$xml= simplexml_load_file("Resources/about_" . $_SESSION['lang'] . ".xml") or die("Error: Could not load the home xml data!");
+$footer= simplexml_load_file("Resources/footer_" . $_SESSION['lang'] . ".xml") or die("Error: Could not load the footer xml data!");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" href="ais.css">
 </head>
 <body>
-	<h1>About</h1>
+	<h1><?php echo $xml->title->__toString(); ?></h1>
+	<p><?php echo $xml->text->__toString(); ?></p>
 	<div class="university-footer navLinks">
-		<a href="home.php">Home</a>
+		<a href="home.php"><?php echo $footer->home->__toString(); ?></a>
 		|
-		<a href="about.php">About</a>
+		<a href="about.php"><?php echo $footer->about->__toString(); ?></a>
 		|
-		<a href="modules.php">Modules</a>
+		<a href="modules.php"><?php echo $footer->modules->__toString(); ?></a>
 		|
-		<a href="resources.php">Resources</a>
+		<a href="resources.php"><?php echo $footer->resources->__toString(); ?></a>
 		|
-		<a href="quiz.php">Quiz</a>
+		<a href="quiz.php"><?php echo $footer->quiz->__toString(); ?></a>
 	</div>
 </body>
 </html>
