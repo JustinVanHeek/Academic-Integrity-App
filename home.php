f53cc87fb4aa6173798c49251a26dcace854d897
Justin van Heek <Justin.vanHeek@mytwu.ca>
2017-05-15 11:59:34 -0700

    Working home.php

--- home.php
+++ home.php
@@ -1,29 +1,7 @@
 <?php
 	session_start();
 
-$xml= simplexml_load_file("Resources/home_" . $_SESSION['lang'] . ".xml") or die("Error: Could not load the login xml data!");
-
-function getTitle() {
-	echo $xml->title->__toString();
-}
-function getLogo() {
-	echo '"' . $xml->logo->__toString() . '"';
-}
-function getAbout() {
-	echo $xml->buttons->about->__toString();
-}
-function getModules() {
-	echo $xml->buttons->modules->__toString();
-}
-function getResources() {
-	echo $xml->buttons->resources->__toString();
-}
-function getQuiz() {
-	echo $xml->buttons->quiz->__toString();
-}
-function getProject() {
-	echo $xml->buttons->project->__toString();
-}
+$xml= simplexml_load_file("Resources/home_" . $_SESSION['lang'] . ".xml") or die("Error: Could not load the home xml data!");
 
 ?>
 
@@ -31,29 +9,29 @@ function getProject() {
 <html>
 <head>
 	<link rel="stylesheet" href="ais.css">
-	<title><?php getTitle(); ?></title>
+	<title><?php echo $xml->title->__toString(); ?></title>
 </head>
 <body>
-	<img src=<?php getLogo(); ?>/>
+	<img src=<?php echo '"' . $xml->logo->__toString() . '"'; ?>>
 	<div class="buttons">
 		<a href="about.html">
-			<button class="home"><?php getAbout(); ?></button>
+			<button class="home"><?php echo $xml->buttons->about->__toString(); ?></button>
 		</a>
 		<a href="/modules.html">
-			<button class="home"><?php getModules(); ?></button>
+			<button class="home"><?php echo $xml->buttons->modules->__toString(); ?></button>
 		</a>
 		<a href="/resources.html">
-			<button class="home"><?php getResources(); ?></button>
+			<button class="home"><?php echo $xml->buttons->resources->__toString(); ?></button>
 		</a>
 		<a href="/quiz.html">
-			<button class="home"><?php getQuiz(); ?></button>
+			<button class="home"><?php echo $xml->buttons->quiz->__toString(); ?></button>
 		</a>
 		<a href="/project.html">
-			<button class="home"><?php getProject(); ?></button>
+			<button class="home"><?php echo $xml->buttons->project->__toString(); ?></button>
 		</a>		
 	</div>
 	<div class="university-footer">
-		<img src="footer.png" />
+		<img src="Assets/footer.png" />
 	</div>
 </body>
 </html>
